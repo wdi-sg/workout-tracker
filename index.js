c57271f99c09dc0bd5207daac75cf59c525eb217
SYNG <simyenng@gmail.com>
2019-10-14 19:53:55 +0800

    Corrected pace calculation Added date input but removed output due to format

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const pg = require('pg');
 
 const configs = {
-user: 'SYNG',
-host: '127.0.0.1',
-database: 'workouts',
-port: 5432,
+    user: 'SYNG',
+    host: '127.0.0.1',
+    database: 'workouts',
+    port: 5432,
 };
 
 const client = new pg.Client(configs);
@@ -17,108 +17,113 @@ console.log("COMPLETE - \'complete\' id time(hhmmss)");
 console.log("---------------------------------------");
 
 const list = function() {
-      let queryList = 'SELECT * FROM workout';
+    let queryText = 'SELECT * FROM workout';
 
-      client.query(queryList, (err, res) => {
+    client.query(queryText, (err, res) => {
         if (err) {
-          console.log("query error", err.message);
+            console.log("query error", err.message);
         } else {
-            const list = res.rows.map( row => {
+            const list = res.rows.map(row => {
                 let id = row.id;
                 let distance = row.distance;
                 let name = row.name;
                 let time = row.time;
                 if (time === null) {
-                    console.log (`${id}. [ ] - ${distance}km - ${name}`);
+                    console.log(`${id}. [ ] - ${distance}km - ${name}`);
                 } else {
-                    console.log (`${id}. [x] - ${distance}km - ${name} - ${time}`);
+                    var hoursMinutes = time.split(/[.:]/);
+                    console.log(hoursMinutes);
+                    let pace = ((parseInt(hoursMinutes[0] * 60) + parseInt(hoursMinutes[1])) / distance).toFixed(2);
+                    //let date = row.created_at;
+                    // let dateFormat = date.getDate() + "-" + date.getMonth() + "-" + date.getFullYear();
+                    console.log(`${id}. [x] - ${distance}km - ${name} - ${time} - ${pace} m/km`);
                 }
             })
         }
         //endConnection();
         // if terminate connection here,
         // cannot continue further commands
-      });
+    });
 }
 list();
 
 const whenConnected = (err) => {
-  if( err ){
-    console.log( "error", err.message );
-  }
+    if (err) {
+        console.log("error", err.message);
+    }
 
-  let action = process.argv[2]
+    let action = process.argv[2]
 
-  switch (action) {
+    switch (action) {
+        // case 'list':
+        //   let queryList = 'SELECT * FROM workout';
 
-    // case 'list':
-    //   let queryList = 'SELECT * FROM workout';
+        //   client.query(queryList, (err, res) => {
+        //     if (err) {
+        //       console.log("query error", err.message);
+        //     } else {
+        //         const list = res.rows.map( row => {
+        //             let id = row.id;
+        //             let distance = row.distance;
+        //             let name = row.name;
+        //             console.log (`${id}. ${distance}km - ${name}`);
+        //         })
+        //     }
+        //     endConnection();
+        //   });
+        //   break;
 
-    //   client.query(queryList, (err, res) => {
-    //     if (err) {
-    //       console.log("query error", err.message);
-    //     } else {
-    //         const list = res.rows.map( row => {
-    //             let id = row.id;
-    //             let distance = row.distance;
-    //             let name = row.name;
-    //             console.log (`${id}. ${distance}km - ${name}`);
-    //         })
-    //     }
-    //     endConnection();
-    //   });
+        case 'add':
+            let distance = process.argv[3];
+            let name = "";
+            for (let i = 4; i < process.argv.length; i++) {
+                name += process.argv[i] + " ";
+            };
+            let addValues = [distance, name];
 
-    //   break;
+            queryText = 'INSERT INTO workout (distance, name) VALUES ($1, $2) RETURNING *';
+            client.query(queryText, addValues, (err, res) => {
+                if (err) {
+                    console.log("query error", err.message);
+                } else {
+                    console.log("Inserting: Distance-" + distance + " Name-" + name);
+                }
+                endConnection();
+            });
+            break;
 
+        case 'complete':
+            let id = process.argv[3];
+            let time = process.argv[4];
+            let date = process.argv[5];
+            let completeValues = [id, time, date];
+            console.log(completeValues);
 
-    case 'add':
-        let distance = process.argv[3];
-        let name = "";
-        for ( let i=4; i<process.argv.length; i++ ) {
-            name += process.argv[i] + " ";
-        };
-        let addValues = [distance, name];
+            queryText = 'UPDATE workout SET time=$2, created_at=$3 WHERE id=$1';
+            //queryText = 'UPDATE workout SET created_at=$3 WHERE id=$1';
+            client.query(queryText, completeValues, (err, res) => {
+                if (err) {
+                    console.log("query error", err.message);
+                } else {
+                    console.log("Completed workout " + id + " in " + time + " on " + date);
+                }
+                endConnection();
+            })
+            break;
 
-        let queryInsert = 'INSERT INTO workout (distance, name) VALUES ($1, $2) RETURNING *';
-        client.query(queryInsert, addValues, (err, res) => {
-            if (err) {
-              console.log("query error", err.message);
-            } else {
-              console.log("Inserting: Distance-" + distance + " Name-" + name);
-            }
-            endConnection();
-        });
-        break;
+        case 'sort':
+            break;
 
-    case 'complete':
-        let id = process.argv[3];
-        let time = process.argv[4];
-        let completeValues = [id, time];
-
-        let queryUpdate = 'UPDATE workout SET time=$2 WHERE id=$1';
-        client.query(queryUpdate, completeValues, (err, res) => {
-            if (err) {
-                console.log("query error", err.message);
-            } else {
-                console.log("Completed workout " + id + " in " + time);
-            }
-            endConnection();
-        })
-        break;
-
-  }
-
-
-
+    }
 }
 
 client.connect(whenConnected);
 
 const endConnection = () => {
     client.end(err => {
-      console.log('Command completed')
-      if (err) {
-        console.log('error during disconnection', err.stack)
-      }
+        console.log('Command completed')
+        if (err) {
+            console.log('error during disconnection', err.stack)
+        }
     })
-};
+}
