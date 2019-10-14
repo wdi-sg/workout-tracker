16397f65a99669d10f670abcc9db6e262eadc752
lhjd <hongjinlau@gmail.com>
2019-10-14 16:29:00 +0800

    Display time

--- index.js
+++ index.js
@@ -26,15 +26,35 @@ const endConnection = () => {
   });
 };
 
-const getOutputText = (res) => {
-    let outputText = "";
-    let workouts = res.rows;
+const getOutputText = res => {
+  let outputText = "";
+  let workouts = res.rows;
+  console.log("workouts", workouts);
 
-    for (let i = 0; i < workouts.length; i++) {
-        outputText += `${i+1}. [ ] - ${workouts[i].distance}km - ${workouts[i].name}\n`;
+  for (let i = 0; i < workouts.length; i++) {
+    let outputTime = "";
+    if (workouts[i].time) {
+      let minutes = "";
+      let seconds = "";
+
+      if (workouts[i].time.includes(".")) {
+        let time = workouts[i].time.split(".");
+        minutes = time[0];
+        seconds = time[1];
+      } else {
+        minutes = workouts[i].time;
+        seconds = "0";
+      }
+      outputTime = ` - ${+minutes <= 9 ? "0" + minutes : minutes}:${
+        +seconds <= 9 ? "0" + seconds : seconds
+      }`;
     }
-    
-    return outputText;
+    outputText += `${i + 1}. ${workouts[i].time ? "[ X ]" : "[   ]"} - ${workouts[i].distance}km - ${
+      workouts[i].name
+    } ${outputTime} \n`;
+  }
+
+  return outputText;
 };
 
 client.connect(err => {
@@ -55,7 +75,7 @@ client.connect(err => {
         endConnection();
       } else {
         let outputText = `Added: ${res.rows[0].distance} "${res.rows[0].name}".`;
-        console.log(outputText)
+        console.log(outputText);
         endConnection();
       }
     });
@@ -69,6 +89,7 @@ client.connect(err => {
       } else {
         let outputText = getOutputText(res);
         console.log(outputText);
+        // console.log(res.rows);
 
         endConnection();
       }
