8965be43f0ff0427532630062e0e438292e30b8a
Nathanael Tan <nathanael.tan92@gmail.com>
2019-10-14 19:56:07 +0800

    average speed/pace added to the postgres table

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const configs = {
 
 const client = new pg.Client(configs);
 
+
 const whenQueryDone = (err, result) => {
     
     // let avgTime = results.row
@@ -29,17 +30,18 @@ const whenQueryDone = (err, result) => {
            dash = ""
        } else {
            time = obj.time
-           pace =  obj.time / obj.distance;
+           pace=Math.round(obj["avgspeed"])
+           
        }
    
        
 
-       return `${obj["id"]}. ${box} - ${obj.distance}km - ${obj.name} ${dash} ${time} ${dash} ${pace}${paceUnit}   `
+       return `${obj["id"]}. ${box} - ${obj.distance}km - ${obj.name} ${dash} ${time} ${dash} ${pace}${paceUnit}`
 
        
    })
 //   console.log("results: ", result.rows);
-  console.log(display.join("\n") + `\n AVG TIME: `)
+  console.log(display.join("\n"))
 };
 
 const whenConnected = err => {
@@ -47,38 +49,56 @@ const whenConnected = err => {
     console.log("error INSIDE WHEN CONNECTED", err.message);
   }
   let command = process.argv[2];
+//   ADD A WORKOUT TO THE TABLE
   if (command === "add") {
     let distance = process.argv[3];
     let name = process.argv[4];
 
     let inputValues = [distance, name];
     const text =
-      "INSERT INTO workouts (distance, name) VALUES ($1, $2) RETURNING *";
+      "INSERT INTO workouts (distance, name) VALUES ($1, $2)";
 
     console.log("MY QUERYYYY: " + text);
     client.query(text, inputValues, whenQueryDone);
+
+    // MARK A WORKOUT AS COMPLETE
   } else if(command === "complete"){
     let id = process.argv[3];
     let time = process.argv[4];
     let inputValues = [id, time]
-    const text = "UPDATE workouts SET completed='true', time=$2 WHERE id =$1 RETURNING *"
+    const text = "UPDATE workouts SET completed='true', time=$2, avgspeed=$2/distance WHERE id=$1 RETURNING *"
 
     console.log("MY QUERYYYY: " + text);
     client.query(text, inputValues, whenQueryDone);
 
+    // PERMANENTLY DELETE A WORKOUT FROM THE TABLE
   } else if(command === "delete") {
     let id = process.argv[3];
     let inputValues = [id];
     const text = "DELETE from workouts WHERE id=$1"
     client.query(text, inputValues, whenQueryDone);
 
-}else {
+} else if(command === "clear"){
+    const text = "TRUNCATE workouts RESTART IDENTITY"
+    client.query(text, whenQueryDone);
+
+
+} else {
     const text =
       "SELECT * FROM workouts ORDER BY id ASC;";
 
     console.log("MY QUERYYYY: " + text);
+     
     client.query(text, whenQueryDone);
+   
   }
+ 
 };
 
+
 client.connect(whenConnected);
+
+
+
+
+
