32caec8ad6f22101cc48688ad21269a2426c23b5
dannyozh <danielongzhihao@gmail.com>
2019-10-14 15:28:15 +0800

    integrated "show" and "add" functions together

--- index.js
+++ index.js
@@ -10,43 +10,54 @@ const configs = {
 const client = new pg.Client(configs);
 
 //to log new workout
-// let id = process.argv[2];
-// let distance = process.argv[3];
-// let level = process.argv[4];
+let id = process.argv[2];
+let distance = process.argv[3];
+let level = process.argv[4];
 
-// var inputValue = [id, distance, level];
+var inputValue = [id, distance, level];
 
+client.connect(err => {
+  if (err) {
+    console.log("error", err.message);
+  } else if (process.argv[2] === "show") {
+    let text = "SELECT * FROM workouts";
+    client.query(text, (err, res) => {
+      if (err) {
+        console.log("query error", err.message);
+      } else {
+        for (let i = 0; i < res.rows.length; i++) {
+          console.log("result: " + res.rows[i].id + ". [ ] - " + res.rows[i].distance + " - " + res.rows[i].level);
+        }
+      }
+    });
+  } else {
+    let text = "INSERT INTO workouts (id, distance, level) VALUES ($1, $2, $3);";
+
+    client.query(text, inputValue, (err, res) => {
+      if (err) {
+        console.log("query error", err.message);
+      } else {
+        console.log("logged new exercise!");
+      }
+    });
+  }
+});
+
+// to show all logged workouts
 // client.connect(err => {
 //   if (err) {
 //     console.log("error", err.message);
 //   }
 
-//   let text = "INSERT INTO workouts (id, distance, level) VALUES ($1, $2, $3);";
+//   let text = "SELECT * FROM workouts";
 
-//   client.query(text, inputValue, (err, res) => {
+//   client.query(text, (err, res) => {
 //     if (err) {
 //       console.log("query error", err.message);
 //     } else {
-//       console.log("logged new exercise!");
+//       for (let i = 0; i < res.rows.length; i++) {
+//         console.log("result: " + res.rows[i].id + ". [ ] - " + res.rows[i].distance + " - " + res.rows[i].level);
+//       }
 //     }
 //   });
 // });
-
-// to show all logged workouts
-client.connect(err => {
-  if (err) {
-    console.log("error", err.message);
-  }
-
-  let text = "SELECT * FROM workouts";
-
-  client.query(text, (err, res) => {
-    if (err) {
-      console.log("query error", err.message);
-    } else {
-      for (let i = 0; i < res.rows.length; i++) {
-        console.log("result: " + res.rows[i].id + ". [ ] - " + res.rows[i].distance + " - " + res.rows[i].level);
-      }
-    }
-  });
-});
