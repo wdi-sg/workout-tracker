2929e81b47683cab285e9bfdee215bae39b2c8c7
“woofborn” <wolf.born@gmail.com>
2019-10-14 16:57:20 +0800

    Refactored - show/add/complete as different functions

--- index.js
+++ index.js
@@ -9,39 +9,49 @@ port: 5432,
 
 const client = new pg.Client(configs);
 
+let queryText;
+let checkOff = "[ ]"
+let distance = process.argv[3];
+let name = process.argv[4];
+let command = process.argv[2];
+
 client.connect((err) => {
 
       if( err ){
         console.log( "error", err.message );
       }
 
-      let queryText;
-
-      let checkOff = "[ ]"
-
-
-      let distance = process.argv[3];
-      let name = process.argv[4];
-
-      let command = process.argv[2];
-
       switch (command){
-        case "add":
-        queryText = 'INSERT INTO workouts(complete,distance,name) VALUES ($1, $2,$3) RETURNING *';
+            case "add":
+            addWorkout();
+            break;
+
+            case "show":
+           showWorkout()
+            break;
+
+            case "complete":
+            completeWorkout()
+      }
+})
+
+function addWorkout() {
+    queryText = 'INSERT INTO workouts(complete,distance,name) VALUES ($1, $2,$3) RETURNING *';
               const values = [checkOff, distance, name]
         client.query(queryText, values, (err,res)=>{
-        if (err){
-            console.log("query error", err.message);
-            } else {
-              // iterate through all of your results:
+            if (err){
+                console.log("query error", err.message);
+                } else {
+                  // iterate through all of your results:
 
-                console.log("added workout: ", `${res.rows[0].id}. ${res.rows[0].complete} - ${res.rows[0].distance}km - ${res.rows[0].name}`);
-        }
-    })
-        break;
+                    console.log("added workout: ", `${res.rows[0].id}. ${res.rows[0].complete} - ${res.rows[0].distance}km - ${res.rows[0].name}`);
+            }
+        })
 
-        case "show":
-        queryText = 'SELECT * FROM workouts ORDER BY id ASC';
+}
+
+function showWorkout(){
+ queryText = 'SELECT * FROM workouts ORDER BY id ASC';
         client.query(queryText, (err,res)=>{
            if (err) {
           console.log("query error", err.message);
@@ -51,30 +61,18 @@ client.connect((err) => {
             }
           }
         });
-        break;
 
-        case "complete":
+}
 
-        queryText = `UPDATE workouts SET complete = '[x]', time = '${process.argv[4]}' WHERE id = ${process.argv[3]} RETURNING *`
+function completeWorkout(){
+     queryText = `UPDATE workouts SET complete = '[x]', time = '${process.argv[4]}' WHERE id = ${process.argv[3]} RETURNING *`
         client.query(queryText, (err,res)=>{
-        if (err){
-            console.log("query error", err.message);
-            } else {
-              // iterate through all of your results:
+            if (err){
+                console.log("query error", err.message);
+                } else {
+                  // iterate through all of your results:
 
-                console.log("added workout: ", `${res.rows[0].id}. ${res.rows[0].complete} - ${res.rows[0].distance}km - ${res.rows[0].name} - ${res.rows[0].time}`);
-        };
-
-    })
-
-
-
-
-
-      }
-
-
-
-
-
-})
+                    console.log("completed workout: ", `${res.rows[0].id}. ${res.rows[0].complete} - ${res.rows[0].distance}km - ${res.rows[0].name} - ${res.rows[0].time}`);
+            };
+        })
+}
