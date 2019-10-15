0f368572b7b2918bb4fc653e94b4eeeb32cdb444
“kellylimmm” <kellylim.06@gmail.com>
2019-10-15 09:02:16 +0800

    update

--- nodepost/index.js
+++ nodepost/index.js
@@ -9,7 +9,10 @@ port: 5432,
 
 const client = new pg.Client(configs);
 
-// let queryText = 'SELECT * FROM workout';
+let queryText = 'SELECT * FROM workouts';
+
+var commandType = process.argv[2];
+console.log("Your command is: "+commandType);
 
 const whenQueryDone =(err, result) => {
   if (err){
@@ -25,36 +28,61 @@ const whenQueryDone =(err, result) => {
 const whenConnected = (err) => {
 
 
-    // client.query(queryText, (err, res) => {
-    // if (err) {
-    //   console.log("query error", err.message);
-    // } else {
-    //       // iterate through all of your results:
-    //       for( let i=0; i<res.rows.length; i++ ){
-    //         console.log("result: ", res.rows[i]);
-    //       }
-    //     }
-    // });
+    client.query(queryText, (err, res) => {
+    if (err) {
+      console.log("query error", err.message);
+    }
 
-  let distance = process.argv[2];
-  let name = process.argv[3];
+  if (commandType === "add") {
+    var commandText = process.argv[3];
+    console.log("Your command text is " + commandText);
+         if (!res["todoList"]){
+            res["todoList"] = [];
+        };
 
-  var inputValues = [distance, name];
+  res["todoList"].push(commandText);
+  var objectList = res["todoList"];
+  console.log("AAA " + objectList);
 
-  const text = "INSERT INTO workouts (distance,name) VALUES ($1, $2) RETURNING *";
+}
 
-  console.log("MY QUERYYYY: "+text);
+    else {
+          // iterate through all of your results:
+          for( let i=0; i<res.rows.length; i++ ){
+            console.log( (i + 1) + "." + "[ ]" + " - " + objectList[i]);
+          }
+        }
+    });
 
-  client.query(text, inputValues, whenQueryDone);
+//======
+
+    let distance = process.argv[3];
+    let name = process.argv[4];
+
+    var inputValues = [distance, name];
+
+    const text = "INSERT INTO workouts (distance,name) VALUES ($1, $2) RETURNING *";
+
+    console.log("MY QUERYYYY: "+text);
+
+    client.query(text, inputValues, whenQueryDone);
+
 
   if( err ){
-      console.log( "error INSIDE WHEN CONNECTED", err.message );
-    }
+        console.log( "error INSIDE WHEN CONNECTED", err.message );
+      }
+
+
+//===================
+
+
+
+
+
 
 
 
 };
-
 
 
 client.connect(whenConnected);
